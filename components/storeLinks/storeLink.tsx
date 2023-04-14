b137407c5f991b47046997ada68754b9cb5f46d0
agredo-alejo <agredoalejo@gmail.com>
2023-04-13 23:07:08 -0400

    added vercel analytics

--- components/storeLinks/storeLink.tsx
+++ components/storeLinks/storeLink.tsx
@@ -14,7 +14,7 @@ function StoreLink({
    return (
       <Link
          href={href}
-         className={`relative flex items-center bg-[#d0fef7] py-2 w-[90vw]  px-4 rounded-full border-2 border-black hover:scale-[1.02] ${className}`}
+         className={`relative flex items-center bg-[#d0fef7] py-2 w-[90vw] max-w-2xl  px-4 rounded-full border-2 border-black hover:scale-[1.01] ${className}`}
       >
          <span className="relative aspect-square flex w-[3.5rem]">
             <Image src="/EmokushopLogo.png" alt="Emokushop logo" fill />
